50b40007eec41413ab388fc7288f15e4f8067d73
rahul <rahul.valasai@gmail.com>
2022-09-06 22:25:23 +0530

    props-example.

---
+++ props/component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Component = () => {
+  return (
+    <div>
+    <input type={'text'} />
+    <button style={{marginLeft:'100px'}}></button>
+    </div>
+  )
+}
+
+export default Component
